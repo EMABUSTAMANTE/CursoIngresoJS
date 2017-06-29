c709bb564bf6ede31aed47bd11b446cdd5e55c57
EMABUSTAMANTE <emabustamante.27@gmail.com>
2017-06-28 22:12:04 -0300

    Commit 2
    Ejercicios después del recreo

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,13 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'MOSTRAR'*/
+function Mostar()
+{
+	//promt se usa para guardar datos, alert para mostrar//
+	var dato=0; //con dato le doy el valor
+	//dato=document.getElementById('elNombre').value; . esto sirve para obtener informacion del usuario DNI, NOMBRE, ETC
+	//alert(dato);
+	document.getElementById('elNombre').value="Natalia";
+	alert('elNombre')
+}
+
+
