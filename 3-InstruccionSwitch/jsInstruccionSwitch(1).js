cef23819d83fcbff385bc1c00ba624b8746bd539
Emanuel Moreira <emabustamante.27@gmail.com>
2017-07-07 22:12:44 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -3,7 +3,41 @@ function Mostrar()
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
 
+//if(mesDelAño=="Enero")
+	{
+	//	alert("Que comiences bien el año");
+	}
+//else
+	//if (mesDelAño=="Marzo") 
+	{
+	//	alert("A clases");
+	}
+	//else
+	//	if (mesDelAño=="Julio") 
+	{
+	//	alert("Se vienen las vacaciones");
+	}
+	//	else
+	//		if(mesDelAño=="Diciembre")
+			{
+	//			alert("Felices Fiestas!!!");
+			}
 
-
+switch(mesDelAño) 
+{
+    case "Enero":
+        alert("Que comiences bien el año");
+        break;
+    case "Marzo":
+        alert("A clases");
+        break;
+    case "Julio":
+    	alert("Se vienen las vacaciones");
+    	break;
+    case "Diciembre":
+    	alert("Felices Fiestas!!!");
+    	break;
+    default:
+}
 
 }//FIN DE LA FUNCIÓN
